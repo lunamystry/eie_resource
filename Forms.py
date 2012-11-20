96c70e544eaa04ec2cf40dec14c8b54d188d646f
Mandla Mbuli <lm.mbuli@gmail.com>
2012-11-20 13:55:33 +0200

    Added more body to ip registration

--- Forms.py
+++ Forms.py
@@ -2,8 +2,15 @@ from flask.ext.wtf import Form, TextField, Required
 
 class RegistrationForm(Form):
     name = TextField("Name", validators=[Required()])
+    student_number = TextField("Student number", validators=[Required()])
+    yos = TextField("Year of study", validators=[Required()])
     email = TextField("email", validators=[Required()])
     mac = TextField("mac address", validators=[Required()])
     domain = TextField("Domain", validators=[Required()])
     supervisor_name = TextField("Supervisor Name", validators=[Required()])
-    supervisor_email = TextField("email", validators=[Required()])
+    supervisor_email = TextField("Supervisor email", validators=[Required()])
+
+class LockerRegistrationForm(Form):
+    name = TextField("Name", validators=[Required()])
+    student_number = TextField("Student number", validators=[Required()])
+    email = TextField("email", validators=[Required()])
