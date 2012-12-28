00bee178b2dd5ac0bd3c7735235c73b7c7f75c08
Mandla Mbuli <lm.mbuli@gmail.com>
2012-12-28 23:11:01 +0200

    Got ldap working on dev machine and added more play code

--- scripts/manage_ldap.py
+++ scripts/manage_ldap.py
@@ -2,12 +2,22 @@
 #  ldapsearch -xLLL -H <host> -b <base> <search>
 
 import ldap
+from ConfigParser import RawConfigParser
 
-server = 'eieldap.eie.wits.ac.za'
-dn = "cn=admin,dc=eie,dc=wits,dc=ac,dc=za"
-pw = "" # TODO: Find a better way to hold the password, read it from?
+# You can pass any file-like object; if it has a name attribute,
+# that name is used when file format error messages are printed
+cfg = RawConfigParser()
+cfg.read('../resource.cfg')
+server = cfg.get("ldap","server")
+dn = cfg.get("ldap","dn")
+pw = cfg.get("ldap","pw")
+
 con = ldap.initialize('ldap://'+server)
 con.simple_bind_s(dn,pw)
+r = con.search_s('dc=eie,dc=wits,dc=ac,dc=za',ldap.SCOPE_SUBTREE)
+for dn,entry in r:
+    print 'Processing',repr(dn)
+print con.compare_s('cn=leny,dc=eie,dc=wits,dc=ac,dc=za', 'cn', 'leny')
 con.unbind()
 
 def change_password():
