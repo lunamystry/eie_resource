c19bf06d49574b037cbe8219c3bf2ddef6286c62
Mandla Mbuli <lm.mbuli@gmail.com>
2012-12-29 22:41:19 +0200

    Added distribute setup.py and documentation

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='EIE Resource frontend',
+    version='0.1',
+    long_description=__doc__,
+    packages=['resource'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'Flask>=0.9',
+        'Jinja2>=2.6',
+        'Hamlish-Jinja>=0.2.0',
+        'Flask-KVSession>=0.3.2',
+        'Flask-WTF>=0.8',
+        'WTForms>=1.0.2',
+        'Werkzeug>=0.8.3',
+        'python-ldap>=2.4.10',
+        'Flask-Login>=0.1.3',
+        'SQLAlchemy>=0.6']
+    )
