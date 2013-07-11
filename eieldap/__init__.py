43a0b981a924312c1a23f4f04caca69053afaaf1
Mandla Mbuli <lm.mbuli@gmail.com>
2013-07-11 11:50:27 +0200

    Added some code for configuration storage

--- eieldap/__init__.py
+++ eieldap/__init__.py
@@ -1,3 +1,24 @@
-import directory
-import xlstoldif
+import os
+# import manager
+# import models
+# import xlstoldif
 
+config= None
+
+# config= None
+# for loc in os.curdir, os.path.expanduser("~"), "/etc/myproject", os.environ.get("MYPROJECT_CONF"):
+#     try:
+#         with open(os.path.join(loc,"myproject.conf")) as source:
+#             config.readfp( source )
+#     except IOError:
+#         pass
+
+for loc in os.curdir, "/etc/eieldap":
+    try:
+        with open(os.path.join(loc,".eieldaprc")) as source:
+            print(source)
+            # config.readfp( source )
+    except IOError:
+        pass
+
+# manager = Manager(source)
