a11fc94c999d285874c20357e784d2445ea65035
Mandla Mbuli <lm.mbuli@gmail.com>
2013-07-14 16:09:53 +0200

    Added a logger

--- eieldap/__init__.py
+++ eieldap/__init__.py
@@ -1,5 +1,8 @@
 import os
+import logging
+import logging.config
 from ConfigParser import SafeConfigParser
+
 
 config = SafeConfigParser()
 for location in os.curdir, "/etc/eieldap":
@@ -7,4 +10,11 @@ for location in os.curdir, "/etc/eieldap":
         with open(os.path.join(location, "eieldaprc")) as source:
             config.readfp(source)
     except IOError:
-        print "Configuration not found, Searched: " + location
+        logging.debug(" * No application configuration in: " + location)
+    try:
+        with open(os.path.join(location, "logrc")) as source:
+            logging.config.fileConfig(source)
+    except IOError:
+        logging.debug(" * No logging configuration in: " + location)
+
+logger = logging.getLogger(__name__)
