57f09554a8b3cf1bbebb2cd85c6801c0bd627f8a
Mandla Mbuli <lm.mbuli@gmail.com>
2013-07-25 21:27:54 +0200

    Removed mysql from requirements

--- setup.py
+++ setup.py
@@ -17,7 +17,5 @@ setup(
         'Werkzeug>=0.8.3',
         'python-ldap>=2.4.10',
         'Flask-Login>=0.1.3',
-        'SQLAlchemy>=0.6',
-        'SQLAlchemy-Migrate>=0.6',
         'Fabric>=1.5.1']
     )
