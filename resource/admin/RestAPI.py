86034fc59b877932352d2a62db064310e5f4495b
Mandla Mbuli <lm.mbuli@gmail.com>
2013-08-09 09:49:24 +0200

    Started adding machines to the database

--- resource/admin/RestAPI.py
+++ resource/admin/RestAPI.py
@@ -85,3 +85,69 @@ class Users(Resource):
             if field_errors:
                 errors[key] = field_errors
         return data, errors
+
+
+class Machine(Resource):
+    def get(self, machine_id):
+        machine = models.Machine().find_one({"machine_id": machine_id})
+        return machine
+
+    def delete(self, machine_id):
+        client.resource.machines.remove({'id': ObjectId(machine_id)})
+        return "", 204
+
+    def put(self, machine_id):
+        machine = models.Machine().find_one({"machine_id": machine_id})
+        args = request.json
+        data, errors = Machines().validate(args)
+        if errors:
+            return errors, 500
+        machines.save(data)
+        return machine, 201
+
+
+class Machines(Resource):
+    def get(self):
+        machines = models.Machine().find()
+        return jsonify({"result": machines})
+
+    def post(self):
+        machines = models.Machine().find()
+        args = request.json
+        data, errors = self.validate(args)
+        if errors:
+            return errors, 500
+        if models.Machine().save(data):
+            return args, 201
+        else:
+            return "Machine could not be created " + str(args), 500
+
+    def validate(self, data):
+        errors = {}
+        validators = {"first_name": [required],
+                      "last_name": [required],
+                      "machinename": [required,
+                                   functools.partial(length, min=3)],
+                      "student_no": [],
+                      "home_directory": [],
+                      "login_shell": [],
+                      "yos": [],
+                      "email": []}
+        # make sure all value keys are there
+        for key in validators.keys():
+            try:
+                value = data[key]
+            except KeyError:
+                data[key] = None
+        # validate
+        for key in validators.keys():
+            value = data[key]
+            field_errors = []
+            for validator in validators[key]:
+                try:
+                    validator(value)
+                except ValidationError as e:
+                    field_errors.append(e.message)
+            if field_errors:
+                errors[key] = field_errors
+        return data, errors
