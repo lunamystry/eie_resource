b9dd87bd5e9f43aac95cf3b6b02c05d1955275a1
Mandla Mbuli <lm.mbuli@gmail.com>
2013-10-05 18:17:46 +0200

    Starred adding Groups

--- eieldap/models.py
+++ eieldap/models.py
@@ -5,7 +5,6 @@ import subprocess
 
 
 class User():
-
     def __init__(self, manager=None):
         if manager is None:
             manager = Manager(config)
@@ -108,6 +107,39 @@ class User():
             return new_user
         else:
             return user
+
+
+class Groups():
+    def __init__(self, manager=None):
+        if manager is None:
+            manager = Manager(config)
+        self.manager = manager
+        self.basedn = "ou=groups," + self.manager.base
+
+    def save(self, attr):
+        """ if the machine exists update, if not create"""
+        machine = manager.find_one(attr["dn"])
+        if machine:
+            manager.update(attr)
+            return attr["dn"]
+        else:
+            manager.create(attr)
+            return attr["dn"]
+        return "error"
+
+    def delete(self, uid):
+        """ Deletes a machine """
+        dn = "uid=" + uid + "," + self.basedn
+        #TODO: errors
+        self.manager.delete(dn)
+
+    def find(self):
+        """ Returns all the people in the directory (think ldap)"""
+        return self.manager.find(self.basedn)
+
+    def find_one(self, attr):
+        """ Returns a single machine """
+        return self.manager.find_one(attr, self.basedn)
 
 
 class Machines():
