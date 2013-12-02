643a4e3da47193fa5c1527ad45380475728b31d8
Mandla Mbuli <lm.mbuli@gmail.com>
2013-12-02 12:04:57 +0200

    updated add_user script

--- eieldap/scripts/add_user.py
+++ eieldap/scripts/add_user.py
@@ -1,12 +1,13 @@
 from eieldap.models import users
 
-user = {"username": "mbulil",
-        "first_name": "Leonard",
-        "last_name": "Mbuli",
-        "email": ["0705871Y@students.wits.ac.za"],
+user = {"username": "guneap",
+        "first_name": "Gunea",
+        "last_name": "Pig",
+        "email": ["gunea.pig@students.wits.ac.za"],
         "password": "passing",
         "hosts": ['babbage.ug.eie.wits.ac.za',
-                  'testing.ug.eie.wits.ac.za'],
+                  'testing.ug.eie.wits.ac.za',
+                    'volt.eie.wits.ac.za'],
         "yos": "4"}
 
 if users.save(user):
