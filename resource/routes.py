f3bfa82da310e5d168e251f76fccee4b9f857ec5
Mandla Mbuli <lm.mbuli@gmail.com>
2014-01-26 10:17:23 +0200

    templates are in the right directory now

--- resource/routes.py
+++ resource/routes.py
@@ -32,12 +32,12 @@ def index():
 
 @app.errorhandler(404)
 def page_not_found(error):
-    return render_template('404.html'), 404
+    return render_template('templates/404.html'), 404
 
 
 @app.errorhandler(500)
 def internal_error(error):
-    return render_template('500.html'), 500
+    return render_template('templates/500.html'), 500
 
 
 # LOGIN -----------------------------------------------
