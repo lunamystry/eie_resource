360a8de5def06951d002464acbec8387e39bc58b
Mandla Mbuli <lm.mbuli@gmail.com>
2014-01-26 09:24:42 +0200

    Removed admin specific things from the guest frontend

--- resource/frontend/app/js/app.js
+++ resource/frontend/app/js/app.js
@@ -9,13 +9,11 @@ angular.module('resource', [
   'app.directives',
   'app.controllers',
 ]).
-  config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/home', {templateUrl: 'partials/home.html', controller: 'homeCtrl'});
-    $routeProvider.when('/users', {templateUrl: 'partials/users.html', controller: 'usersCtrl'});
-    $routeProvider.when('/users/:username', {templateUrl: 'partials/user_edit.html', controller: 'userEditCtrl'});
-    $routeProvider.when('/bookings', {templateUrl: 'partials/bookings.html', controller: 'bookingsCtrl'});
-    $routeProvider.when('/class_photos', {templateUrl: 'partials/class_photos.html', controller: 'classPhotosCtrl'});
-    $routeProvider.when('/about', {templateUrl: 'partials/about.html', controller: 'aboutCtrl'});
-    $routeProvider.when('/login', {templateUrl: 'partials/login.html', controller: 'loginCtrl'});
-    $routeProvider.otherwise({redirectTo: '/home'});
-  }]);
+config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/home', {templateUrl: 'partials/home.html', controller: 'homeCtrl'});
+  $routeProvider.when('/bookings', {templateUrl: 'partials/bookings.html', controller: 'bookingsCtrl'});
+  $routeProvider.when('/class_photos', {templateUrl: 'partials/class_photos.html', controller: 'classPhotosCtrl'});
+  $routeProvider.when('/about', {templateUrl: 'partials/about.html', controller: 'aboutCtrl'});
+  $routeProvider.when('/login', {templateUrl: 'partials/login.html', controller: 'loginCtrl'});
+  $routeProvider.otherwise({redirectTo: '/home'});
+}]);
