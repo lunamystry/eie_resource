deca802807faec1c693619f40a7e24210024c208
Mandla Mbuli <lm.mbuli@gmail.com>
2014-02-05 12:05:30 +0000

    updated version number to 0.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='Resource',
-    version='0.1',
+    version='0.5',
     long_description=__doc__,
     packages=['resource'],
     include_package_data=True,
