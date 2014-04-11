907075d5da27ca70903a4c0199f79089503461fb
Mandla Mbuli <lm.mbuli@gmail.com>
2014-04-11 20:29:39 +0000

    moved the usersCtrl into a separate file

--- resource/admin/frontend/app/js/app.js
+++ resource/admin/frontend/app/js/app.js
@@ -10,10 +10,17 @@ angular.module('resource', [
   'app.services',
   'app.directives',
   'app.controllers',
+  'controller.users',
 ]).
   config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/home', {templateUrl: 'views/home.html', controller: 'homeCtrl'});
-    $routeProvider.when('/users', {templateUrl: 'views/users.html', controller: 'usersCtrl'});
-    $routeProvider.when('/users/:username', {templateUrl: 'views/user_edit.html', controller: 'userEditCtrl'});
+      $routeProvider.when('/home', {
+          templateUrl: 'views/home.html',
+          controller: 'homeCtrl'});
+      $routeProvider.when('/users', {
+          templateUrl: 'views/users.html',
+          controller: 'usersCtrl'});
+      $routeProvider.when('/users/:username', {
+          templateUrl: 'views/user_edit.html',
+          controller: 'userEditCtrl'});
     $routeProvider.otherwise({redirectTo: '/home'});
   }]);
