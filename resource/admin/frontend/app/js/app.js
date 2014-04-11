e8ec155105ab0a1b8170facfe4cac90669f26e50
Mandla Mbuli <lm.mbuli@gmail.com>
2014-04-11 18:54:09 +0000

    renamed partials to views

--- resource/admin/frontend/app/js/app.js
+++ resource/admin/frontend/app/js/app.js
@@ -12,8 +12,8 @@ angular.module('resource', [
   'app.controllers',
 ]).
   config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/home', {templateUrl: 'partials/home.html', controller: 'homeCtrl'});
-    $routeProvider.when('/users', {templateUrl: 'partials/users.html', controller: 'usersCtrl'});
-    $routeProvider.when('/users/:username', {templateUrl: 'partials/user_edit.html', controller: 'userEditCtrl'});
+    $routeProvider.when('/home', {templateUrl: 'views/home.html', controller: 'homeCtrl'});
+    $routeProvider.when('/users', {templateUrl: 'views/users.html', controller: 'usersCtrl'});
+    $routeProvider.when('/users/:username', {templateUrl: 'views/user_edit.html', controller: 'userEditCtrl'});
     $routeProvider.otherwise({redirectTo: '/home'});
   }]);
