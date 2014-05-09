a9d368c5f4cd4f09f1372086d0655b082c76581a
Mandla Mbuli <lm.mbuli@gmail.com>
2014-05-09 17:12:24 +0000

    Moved the admin frontend into the new admin directory

--- resource/admin/__init__.py
+++ resource/admin/__init__.py
@@ -2,8 +2,8 @@ from flask import Blueprint
 
 admin = Blueprint('admin',
                   __name__,
-                  template_folder='frontend/app',
+                  template_folder='../../frontend/admin',
                   static_url_path='',
-                  static_folder='frontend/app')
+                  static_folder='../../frontend/admin')
 
 import resource.admin.routes
