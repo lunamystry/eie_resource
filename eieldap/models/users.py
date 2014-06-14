a04dd5b7ca1e773811db40c27298e8f8bfdc3070
Mandla Mbuli <lm.mbuli@gmail.com>
2014-06-14 10:06:24 +0000

    rethinking users again

--- eieldap/models/users.py
+++ eieldap/models/users.py
@@ -1,39 +1,102 @@
 from eieldap import manager
-from eieldap import config
 import subprocess
-import ldap
 import logging
 import time
 
-logger = logging.getLogger("eieldap.manager")
 # get email template with a lot of very useful information on it
 # lecturer - 6000 -> 6099
 # admin - 6100 -> 6199
 # technical - 6200 -> 6299
 # separate home directories for staff
 
-basedn = "ou=people," + manager.base
-keymap = {"uid": "username",
-          "cn": "first_name",
-          "sn": "last_name",
-          "yos": "yos",
-          "homeDirectory": "home_directory",
-          "loginShell": "login_shell",
-          "uidNumber": "uid_number",
-          "gidNumber": "gid_number",
-          "host": "hosts",
-          "mail": "email"}
-inv_keymap = {}
-for k,v in keymap.items():
-    inv_keymap[v] = k
+logger = logging.getLogger("eieldap.models.users")
+BASEDN = "ou=people," + manager.base
+FROM_LDAP_MAP = {"uid": "username",
+                 "cn": "first_name",
+                 "sn": "last_name",
+                 "homeDirectory": "home_directory",
+                 "loginShell": "login_shell",
+                 "uidNumber": "uid_number",
+                 "gidNumber": "gid_number",
+                 "host": "hosts",
+                 "mail": "email"}
+TO_LDAP_MAP = {}
+for k, v in FROM_LDAP_MAP.items():
+    TO_LDAP_MAP[v] = k
+
+
+class User():
+    """This encapsulates converting and validating the user"""
+    dn = None
+    attributes = {"objectClass": ["inetOrgPerson", "organizationalPerson",
+                  "posixAccount", "sambaSamAccount", "hostObject"],
+                  "uidNumber": None,
+                  "gidNumber": None,
+                  "homeDirectory": None,
+                  "loginShell": "/bin/bash",
+                  "displayName": None,
+                  "sambaSID": None,
+                  "sambaAcctFlags": "[U         ]",
+                  "sambaNTPassword": None,
+                  "sambaLMPassword": None}
+
+    def __init__(self, attr):
+        validate(attr)
+        self.set_attributes(attr)
+
+    def set_attributes(self, attr):
+        '''Set the attributes'''
+        self.dn = "uid=" + attr["username"] + "," + BASEDN
+        if int(attr['yos']) < 5:
+            home_base = "/home/ug"
+        elif int(attr['yos']) == 5:
+            home_base = "/home/pg"
+        elif int(attr['yos']) == 6:
+            home_base = "/home/staff"
+        elif int(attr['yos']) == 7:
+            home_base = "/dev/null"
+        else:
+            error_msg = "Invalid Year of Study {}".format(attr['yos'])
+            logger.error(error_msg)
+            raise TypeError(error_msg)
+        self.attributes['uidNumber'] = next_uid_number(int(attr['yos']))
+        self.attributes['gidNumber'] = user_gid_number(int(attr['yos']))
+        lm_password, nt_password = smb_encrypt(attr["password"])
+        self.attributes['sambaNTPassword'] = nt_password
+        self.attributes['sambaLMPassword'] = lm_password
+        smbRid = self.attributes['uidNumber']*4
+        self.attributes["sambaSID"] = "S-1-5-21-3949128619-541665055-2325163404-{}".format(str(smbRid))
+        self.attributes["homeDirectory"] = "{0}/{1}".format(home_base,
+                                                            attr["username"])
+        self.attributes["loginShell"] = "/bin/bash"
+        self.attributes["displayName"] = "{0} {1}".format(attr["first_name"],
+                                                          attr["last_name"])
+        try:
+            for index, host in enumerate(attr["host"]):
+                self.attributes["hosts"][index] = str(host)
+        except KeyError:
+            pass
+
+    def to_dict(self):
+        convert(self.attributes, FROM_LDAP_MAP)
+
+
+def add(attr):
+    """ adds a new user """
+    user = User(attr)
+    if manager.create(user.attributes['dn'], user.to_dict()):
+        change_password(user.attributes['uid'], None, attr['password'])
+        logger.info("Created user: {}".format(user.attributes['dn']))
+        return True
+    return False
 
 
 def find():
     """ Returns all the people in the directory (think ldap)"""
-    users = manager.find(basedn, filter_key="uid")
+    users = manager.find(BASEDN, filter_key="uid")
     users_list = []
     for user in users:
-        new_user = fix(user, keymap)
+        new_user = convert(user, FROM_LDAP_MAP)
         new_user['yos'] = int(new_user['gid_number'])/1000
         if new_user['email']:
             email = new_user['email'][0]
@@ -42,144 +105,99 @@ def find():
     return users_list
 
 
-def find_one(username=None, attr=None):
-    """ Returns a single user """
-    user = None
-    if username is not None:
-        dn = "uid=" + username + "," + basedn
-        user = manager.find_by_dn(dn)
-    elif attr is not None:
-        fixed_user = fix(attr, inv_keymap)
-        user = manager.find_one(fixed_user, basedn, filter_key="uid")
+# def find_one(username=None, attr=None):
+#     """ Returns a single user """
+#     user = None
+#     if username is not None:
+#         dn = "uid=" + username + "," + BASEDN
+#         user = manager.find_by_dn(dn)
+#     elif attr is not None:
+#         fixed_user = fix(attr, inv_keymap)
+#         user = manager.find_one(fixed_user, BASEDN, filter_key="uid")
+#
+#     if user:
+#         user['yos'] = str(int(user['gidNumber'])/1000)
+#         return fix(user, keymap)
+#
+#
+# def update(attr):
+#     """ updates a user"""
+#     fixed_user = fix(attr, inv_keymap)
+#     dn = "uid=" + fixed_user["uid"] + "," + BASEDN
+#     existing_user = manager.find_one(fixed_user, filter_key="uid")
+#
+#     gid_number = user_gid_number(int(attr['yos']))
+#     if 'yos' in fixed_user:
+#         del fixed_user['yos']
+#     fixed_user["gidNumber"] = str(gid_number)
+#     try:
+#         for index, host in enumerate(fixed_user["host"]):
+#             fixed_user["host"][index] = str(host)
+#     except KeyError:
+#         pass
+#
+#     if manager.update(dn, fixed_user):
+#         logger.info("updated user: " + str(dn))
+#         return True
+#     return False
+#
+#
+# def save(attr):
+#     """ if the user exists update, if not create"""
+#     if 'username' not in attr:
+#         raise TypeError("You have a missing attributes: username")
+#
+#     fixed_user = fix(attr, inv_keymap)
+#     dn = "uid=" + fixed_user["uid"] + "," + BASEDN
+#     existing_user = manager.find_one(fixed_user, filter_key="uid")
+#
+#     if existing_user:
+#         return update(attr)
+#     else:
+#         return add(attr)
+#     return False
+#
+#
+# def delete(username=None, user=None):
+#     """ Deletes a user """
+#     existing_user = None
+#     if username is not None:
+#         dn = "uid=" + username + "," + BASEDN
+#         existing_user = manager.find_by_dn(dn)
+#     elif user is not None:
+#         fixed_user = fix(user, inv_keymap)
+#         dn = "uid=" + fixed_user['uid'] + "," + BASEDN
+#         existing_user = manager.find_one(fixed_user, BASEDN, filter_key="uid")
+#
+#     if existing_user:
+#         if manager.delete(existing_user['dn']):
+#             logger.info("deleted: " + existing_user['dn'])
+#             return True
+#     return False
 
-    if user:
-        user['yos'] = str(int(user['gidNumber'])/1000)
-        return fix(user, keymap)
-
-
-def add(attr):
-    """ adds a new user """
-    required_attributes = ['first_name', 'last_name', 'yos', 'email', 'password']
+def validate(attr):
+    '''Make sure that attributes are all there in the correct form'''
+    required_attributes = ['first_name', 'last_name', 'yos', 'email',
+                           'password']
     for attribute in required_attributes:
         if attribute not in attr:
-            raise TypeError("You have a missing attributes: " + attribute)
-    fixed_user = fix(attr, inv_keymap)
-    dn = "uid=" + fixed_user["uid"] + "," + basedn
-
-    uid_number = next_uid_number(int(attr['yos']))
-    gid_number = user_gid_number(int(attr['yos']))
-    lm_password, nt_password = smb_encrypt(attr["password"])
-    smbRid = uid_number*4
-    if int(attr['yos']) < 5:
-        home_base = "/home/ug"
-    elif int(attr['yos']) == 5:
-        home_base = "/home/pg"
-    elif int(attr['yos']) == 6:
-        home_base = "/home/staff"
-    elif int(attr['yos']) == 7:
-        home_base = "/dev/null"
-    else:
-        error_msg = "trying to save but Year of Study " + attr['yos'] + " is invalid"
-        logger.error(error_msg)
-        raise RuntimeError(error_msg)
-    fixed_user["objectClass"] = ["inetOrgPerson", "organizationalPerson", "posixAccount", "sambaSamAccount", "hostObject"]
-    fixed_user["uidNumber"] = str(uid_number)
-    fixed_user["gidNumber"] = str(gid_number)
-    fixed_user["homeDirectory"] = "/home/ug/" + fixed_user["uid"]
-    fixed_user["loginShell"] = "/bin/bash"
-    fixed_user["displayName"] = fixed_user["cn"] + " " + fixed_user["sn"]
-    fixed_user["sambaSID"] = "S-1-5-21-3949128619-541665055-2325163404-" + str(smbRid)
-    fixed_user["sambaAcctFlags"] = "[U         ]"
-    fixed_user["sambaNTPassword"] = nt_password
-    fixed_user["sambaLMPassword"] = lm_password
-    if 'dn' in fixed_user:
-        del fixed_user['dn']
-    if 'yos' in fixed_user:
-        del fixed_user['yos']
-    try:
-        for index, host in enumerate(fixed_user["host"]):
-            fixed_user["host"][index] = str(host)
-    except KeyError:
-        pass
-    if manager.create(dn, fixed_user):
-        change_password(fixed_user['uid'], None, attr['password'])
-        logger.info("Created user: " + str(dn))
-        return True
-    logger.info("user could not be created: " + str(fixed_user))
-    return False
+            raise TypeError("missing attributes: {}".format(
+                attribute))
 
 
-def update(attr):
-    """ updates a user"""
-    fixed_user = fix(attr, inv_keymap)
-    dn = "uid=" + fixed_user["uid"] + "," + basedn
-    existing_user = manager.find_one(fixed_user, filter_key="uid")
-
-    gid_number = user_gid_number(int(attr['yos']))
-    if 'yos' in fixed_user:
-        del fixed_user['yos']
-    fixed_user["gidNumber"] = str(gid_number)
-    try:
-        for index, host in enumerate(fixed_user["host"]):
-            fixed_user["host"][index] = str(host)
-    except KeyError:
-        pass
-
-    if manager.update(dn, fixed_user):
-        logger.info("updated user: " + str(dn))
-        return True
-    return False
-
-def save(attr):
-    """ if the user exists update, if not create"""
-    if 'username' not in attr:
-        raise TypeError("You have a missing attributes: username")
-
-    fixed_user = fix(attr, inv_keymap)
-    dn = "uid=" + fixed_user["uid"] + "," + basedn
-    existing_user = manager.find_one(fixed_user, filter_key="uid")
-
-    if existing_user:
-        return update(attr)
-    else:
-        return add(attr)
-    return False
-
-
-def delete(username=None, user=None):
-    """ Deletes a user """
-    existing_user = None
-    if username is not None:
-        dn = "uid=" + username + "," + basedn
-        existing_user = manager.find_by_dn(dn)
-    elif user is not None:
-        fixed_user = fix(user, inv_keymap)
-        dn = "uid=" + fixed_user['uid'] + "," + basedn
-        existing_user = manager.find_one(fixed_user, basedn, filter_key="uid")
-
-    if existing_user:
-        if manager.delete(existing_user['dn']):
-            logger.info("deleted: " + existing_user['dn'])
-            return True
-    return False
-
-
-def smb_encrypt(password):
-    """ Calls an smbencrypt which comes with freeradius-utils on Ubuntu
-    to encrypt the password given in smbencrypt form
-    """
-    smbencrypt_output = subprocess.check_output(["smbencrypt", password])
-    lm_password = smbencrypt_output[0:32].strip()
-    nt_password = smbencrypt_output[32:].strip()
-    return lm_password, nt_password
-
-
-def fix(user, keymap):
+def convert(user, keymap):
+    '''DESTRUCTIVE: Converts a user either from LDAP form or to depending on 
+    keymap. If converting keys are not in the keymap passed, they will be
+    removed from the result'''
+    if user is None:
+        return None
     new_user = {}
     for key in keymap:
         if keymap[key] == 'uid_number' or keymap[key] == 'gid_number':
             new_user[keymap[key]] = 0
-        elif keymap[key] == 'hosts' or keymap[key] == 'email' or keymap[key] == 'host':
+        elif (keymap[key] == 'hosts'
+                or keymap[key] == 'email'
+                or keymap[key] == 'host'):
             new_user[keymap[key]] = []
         else:
             new_user[keymap[key]] = None
@@ -193,76 +211,86 @@ def fix(user, keymap):
                 else:
                     new_user[nkey] = str(user[key])
             except KeyError:
-                logger.debug("key not mapped: " + key)
+                pass
     return new_user
+
+
+def smb_encrypt(password):
+    """ Calls an smbencrypt which comes with freeradius-utils on Ubuntu
+    to encrypt the password given in smbencrypt form
+    """
+    smbencrypt_output = subprocess.check_output(["smbencrypt", password])
+    lm_password = smbencrypt_output[0:32].strip()
+    nt_password = smbencrypt_output[32:].strip()
+    return lm_password, nt_password
 
 
 def change_password(username, oldpw, newpw):
     """ User the python ldap function to change the password
     of the user with the supplied username"""
-    dn = "uid=" + username + "," + basedn
+    dn = "uid=" + username + "," + BASEDN
     user = manager.find_by_dn(dn)
     if user:
         lm_password, nt_password = smb_encrypt(newpw)
         user["sambaNTPassword"] = nt_password
         user["sambaLMPassword"] = lm_password
-        logger.debug("Changing password for user with dn: " + str(user['dn']))
         dn = user['dn']
         if manager.update(dn, user):
             return manager.change_password(dn, oldpw, newpw)
     return False
 
 
-def reset_password(username):
-    """ User the python ldap function to reset the password
-    of the user with the supplied username"""
-    newpw = "dlab"+str(time.strftime("%Y"))
-    dn = "uid=" + username + "," + basedn
-    user = manager.find_by_dn(dn)
-    if user:
-        lm_password, nt_password = smb_encrypt(newpw)
-        user["sambaNTPassword"] = nt_password
-        user["sambaLMPassword"] = lm_password
-        logger.debug("Changing password for user with dn: " + str(user['dn']))
-        dn = user['dn']
-        if manager.update(dn, user):
-            return manager.change_password(dn, None, newpw)
-    return False
-
-
-def add_host(username, host_domain):
-    """ Allow the user with username to login to host with host_domain.
-    this assumes the host has been configured to use the host property"""
-    user = find_one(username)
-    if not user:
-        raise ValueError("trying to add host {1} to {0}, but {0} does not exist in the ldap".format(username, host_domain))
-
-    try:
-        if host_domain not in user['hosts']:
-            user['hosts'].append(host_domain)
-            save(user)
-    except KeyError:
-        user['hosts'] = [host_domain]
-        save(user)
-
-
-def remove_host(username, host_domain):
-    """ Disallow a user with username to login into a host with host_domain.
-    this assumes the host has been configured to use the host property"""
-    user = find_one(username)
-    if not user:
-        raise ValueError("trying to add host {1} to {0}, but {0} does not exist in the ldap".format(username, host_domain))
-
-    if host_domain in user['hosts']:
-        user['hosts'].remove(host_domain)
-        save(user)
-    else:
-        logger.debug("host: {0} not found in {1} user".format(host_domain, username))
-
-
-def authenticate(username, password):
-    dn = "uid=" + username + "," + basedn
-    return manager.authenticate(dn, password)
+# def reset_password(username):
+#     """ User the python ldap function to reset the password
+#     of the user with the supplied username"""
+#     newpw = "dlab"+str(time.strftime("%Y"))
+#     dn = "uid=" + username + "," + BASEDN
+#     user = manager.find_by_dn(dn)
+#     if user:
+#         lm_password, nt_password = smb_encrypt(newpw)
+#         user["sambaNTPassword"] = nt_password
+#         user["sambaLMPassword"] = lm_password
+#         logger.debug("Changing password for user with dn: " + str(user['dn']))
+#         dn = user['dn']
+#         if manager.update(dn, user):
+#             return manager.change_password(dn, None, newpw)
+#     return False
+#
+#
+# def add_host(username, host_domain):
+#     """ Allow the user with username to login to host with host_domain.
+#     this assumes the host has been configured to use the host property"""
+#     user = find_one(username)
+#     if not user:
+#         raise ValueError("trying to add host {1} to {0}, but {0} does not exist in the ldap".format(username, host_domain))
+#
+#     try:
+#         if host_domain not in user['hosts']:
+#             user['hosts'].append(host_domain)
+#             save(user)
+#     except KeyError:
+#         user['hosts'] = [host_domain]
+#         save(user)
+#
+#
+# def remove_host(username, host_domain):
+#     """ Disallow a user with username to login into a host with host_domain.
+#     this assumes the host has been configured to use the host property"""
+#     user = find_one(username)
+#     if not user:
+#         raise ValueError("trying to add host {1} to {0}, but {0} does not exist in the ldap".format(username, host_domain))
+#
+#     if host_domain in user['hosts']:
+#         user['hosts'].remove(host_domain)
+#         save(user)
+#     else:
+#         logger.debug("host: {0} not found in {1} user".format(
+#             host_domain, username))
+#
+#
+# def authenticate(username, password):
+#     dn = "uid=" + username + "," + BASEDN
+#     return manager.authenticate(dn, password)
 
 
 def next_uid_number(yos):
@@ -281,22 +309,25 @@ def next_uid_number(yos):
 
     """
     if yos not in range(1, 8):
-        logger.error("Tried to add out of range uid/yos")
-        raise ValueError("The Year of Study: " + str(yos) + " is out of range")
-    all_users = find()
+        logger.error("{} is out of uid/yos range".format(str(yos)))
+        raise ValueError("The Year of Study: {} is out of range".format(
+            str(yos)))
+    all_users = manager.find(BASEDN, filter_key="uid")
     uids = []
     start_uid = yos*1000
     for user in all_users:
         try:
-            uid = int(user['uid_number'])
+            uid = int(user['uidNumber'])
             if uid in range(start_uid, start_uid + 1000):
                 uids.append(uid)
         except KeyError:
-            logger.error(user['username'] + " does not have a uid number")
+            error_msg = "{} does not have a uid number".format(user['uid'])
+            logger.error(error_msg)
+            raise TypeError(error_msg)
     for uid in range(start_uid, start_uid + 1000):
         if uid not in uids:
             return uid
-    error_msg = "All uid numbers have for " + str(yos) + " Year of Study have been depleted"
+    error_msg = "uid numbers have for {} have been depleted".format(str(yos))
     logger.error(error_msg)
     raise RuntimeError(error_msg)
 
--- eieldap/tests/users_model_tests.py
+++ eieldap/tests/users_model_tests.py
@@ -1,28 +1,157 @@
-from mock import patch, Mock
 from eieldap.models import users
 import unittest
 
 
-class UsersTestCase(unittest.TestCase):
-    def test_save(self):
-        """ Create a new user """
-        user_to_save = {"username": "guneap",
-                        "first_name": "Gunea",
-                        "last_name": "Pig",
-                        "email": "guneap@students.wits.ac.za",
-                        "password": "passing",
-                        "yos": "1"}
-        expected_user = {"name": "natsuki", "members": ['mandla', 'leny']}
-        ignored_user = {"name": "natsuki", "members": ['mandla', 'leny']}
-        invalid_user = {"name": "navina"}
+class UserServicesTests(unittest.TestCase):
 
+    def setUp(self):
+        self.valid = {"username": "guneap",
+                      "first_name": "Gunea",
+                      "last_name": "Pig",
+                      "email": "guneap@students.wits.ac.za",
+                      "password": "passing",
+                      "hosts": ['dummy'],
+                      "yos": "1"}
+        self.invalid = {"username": "testuser"}
+
+    def test_next_uid_number_in_range(self):
+        for yos in range(1, 8):
+            next_uid = users.next_uid_number(yos)
+            start = yos*1000
+            end = start + 1000
+            self.assertTrue(next_uid in range(start, end))
+
+    def test_next_uid_number_out_of_range(self):
+        '''
+           1 - first year
+           2 - second year
+           3 - third year
+           4 - fourth year
+           5 - postgrad
+           6 - staff
+           7 - machine
+        '''
+        with self.assertRaises(ValueError):
+            users.next_uid_number(-1)
+        with self.assertRaises(ValueError):
+            users.next_uid_number(8)
+
+    def test_validate_valid(self):
+        users.validate(self.valid)
+
+    def test_validate_invalid(self):
+        with self.assertRaises(TypeError):
+            users.validate(self.invalid)
+
+    def test_convert_from_ldap(self):
+        ldap_attr = {'dn': 'uid=monkeyl,ou=people,dc=eie,dc=wits,dc=ac,dc=za',
+                     'displayName': 'Luffy Monkey',
+                     'uid': 'monkeyl',
+                     'objectClass': ['inetOrgPerson', 'organizationalPerson', 'posixAccount', 'sambaSamAccount', 'hostObject'],
+                     'loginShell': '/bin/bash',
+                     'userPassword': '{SSHA}qGfneJ66q1r+29LF9AO8jaGnOOi1yz+N',
+                     'sambaLMPassword': 'B267DF22CB945E3EAAD3B435B51404EE',
+                     'uidNumber': '3000',
+                     'gidNumber': '3000',
+                     'sambaAcctFlags': '[U         ]',
+                     'sambaSID': 'S-1-5-21-3949128619-541665055-2325163404-12000',
+                     'sn': 'Monkey',
+                     'homeDirectory': '/home/ug/monkeyl',
+                     'host': ['babbage.ug.eie.wits.ac.za'],
+                     'mail': ['0000000@students.ug.eie.wits.ac.za'],
+                     'sambaNTPassword': '36AA83BDCAB3C9FDAF321CA42A31C3FC',
+                     'cn': 'Luffy'}
+        converted_ldap_attr = {'first_name': 'Luffy',
+                               'last_name': 'Monkey',
+                               'username': 'monkeyl',
+                               'login_shell': '/bin/bash',
+                               'uid_number': '3000',
+                               'gid_number': '3000',
+                               'home_directory': '/home/ug/monkeyl',
+                               'hosts': ['babbage.ug.eie.wits.ac.za'],
+                               'email': ['0000000@students.ug.eie.wits.ac.za']}
+        converted = users.convert(ldap_attr, users.FROM_LDAP_MAP)
+        self.assertEquals(converted, converted_ldap_attr)
+
+    def test_convert_to_ldap(self):
+        attr = {'username': 'monkeyl',
+                'login_shell': '/bin/bash',
+                'uid_number': '3000',
+                'gid_number': '3000',
+                'hosts': ['babbage.ug.eie.wits.ac.za'],
+                'last_name': 'Monkey',
+                'home_directory': '/home/ug/monkeyl',
+                'email': ['320983232@students.ug.eie.wits.ac.za'],
+                'first_name': 'Luffy'}
+        expected_attr = {'uid': 'monkeyl',
+                         'loginShell': '/bin/bash',
+                         'uidNumber': '3000',
+                         'gidNumber': '3000',
+                         'host': ['babbage.ug.eie.wits.ac.za'],
+                         'sn': 'Monkey',
+                         'homeDirectory': '/home/ug/monkeyl',
+                         'mail': ['320983232@students.ug.eie.wits.ac.za'],
+                         'cn': 'Luffy'}
+        converted = users.convert(attr, users.TO_LDAP_MAP)
+        self.assertEquals(converted, expected_attr)
+
+    def test_convert_from_ldap_None(self):
+        ''' As long as I can do it I am happy'''
+        users.convert(None, users.BASEDN)
+
+    def test_smb_encrypt(self):
+        '''As long as it can be called'''
+        users.smb_encrypt('password')
+
+    def test_change_password(self):
+        '''this just passes things to the manager'''
+        users.change_password('user', 'old_password', 'new_password')
+
+
+class UsersTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.valid = {"username": "guneap",
+                      "first_name": "Gunea",
+                      "last_name": "Pig",
+                      "email": "guneap@students.wits.ac.za",
+                      "password": "passing",
+                      "hosts": ['dummy'],
+                      "yos": "1"}
+        self.existing_user = {"username": "johnd",
+                              "first_name": "John",
+                              "last_name": "Doe",
+                              "email": "mandla.niigata@students.wits.ac.za",
+                              "password": "passing",
+                              "hosts": ['dummy'],
+                              "yos": "4"}
+        self.missing_attributes = {"username": "johnd",
+                                   "password": "passing",
+                                   "hosts": ['dummy'],
+                                   "yos": "4"}
+
+    # def test_add(self):
+    #     '''simply add a new valid user'''
+    #     self.assertTrue(users.add(self.valid))
+
+    # def test_add_existing(self):
+    #     '''adding existing user should update it'''
+    #     self.assertTrue(users.add(self.existing_user))
+
+    # def test_add_missing_required_attributes(self):
+    #     '''adding existing user should update it'''
+    #     with self.assertRaises(TypeError):
+    #         users.add(self.missing_attributes)
+
+    # def test_save(self):
+    #     """ Create a new user """
+    #     pass
         # with self.assertRaises(TypeError):
         #     users.save(invalid_user)
 
         # Does not exist
-        if users.find_one(user_to_save['username']):
-            users.delete(user=user_to_save)
-        self.assertTrue(users.add(user_to_save))
+        # if users.find_one(user_to_save['username']):
+        #     users.delete(user=user_to_save)
 
         # # exists
         # user_to_save = {"username": "guneap",
@@ -48,19 +177,6 @@ class UsersTestCase(unittest.TestCase):
         # user = users.find_one("guneap")
         # self.assertEquals(user, expected_user)
 
-    # def test_get_next_uid(self):
-    #     """ Manual test, can I get the next uid """
-    #     # check that its withing range
-    #     for yos in range(1, 8):
-    #         next_uid = users.next_uid_number(yos)
-    #         start = yos*1000
-    #         end = start + 1000;
-    #         self.assertTrue(next_uid in range(start, end))
-
-    #     with self.assertRaises(ValueError):
-    #         next_uid = users.next_uid_number(-1)
-    #     with self.assertRaises(ValueError):
-    #         next_uid = users.next_uid_number(8)
 
     #     # raises an error if the number of valid uids has been reached
 
