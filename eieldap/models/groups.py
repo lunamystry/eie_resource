66827eebf5bca1f4c7720e7961ccb34edd473d32
Mandla Mbuli <lm.mbuli@gmail.com>
2014-06-18 19:13:32 +0000

    lets do this step by step

--- eieldap/models/groups.py
+++ eieldap/models/groups.py
@@ -13,17 +13,17 @@ for k, v in TO_LDAP_MAP.items():
     FROM_LDAP_MAP[v] = k
 
 
-def save(group):
-    """ if the group exists update, if not create"""
+def add(group):
+    """adds a new posix group to the LDAP directory"""
     if ("members" not in group
             or type(group['members']) is not list
             or len(group['members']) == 0):
         raise TypeError("You must give atleast one group member")
-    unfixed_group = dict(group)
+    unfixed_group = dict(group)  # I don't want to be editing what I'm given
     unfixed_group['members'] = list(group['members'])
     for i, member_name in enumerate(unfixed_group["members"]):
         # TODO: member[i] = users.find_one(member)["id"]
-        unfixed_group['members'][i] = "uid=" + member_name + "," + users.BASEDN
+        unfixed_group['members'][i] = "memberUid=" + member_name
 
     fixed_group = convert(unfixed_group, FROM_LDAP_MAP)
     dn = "cn=" + fixed_group["cn"] + "," + BASEDN
@@ -47,91 +47,90 @@ def save(group):
 def find():
     """ Returns all the groups in the directory (think ldap)"""
     groups = manager.find(BASEDN, filter_key="cn")
-    groups_list = []
-    for group in groups:
-        try:
-            group['member'] = get_names(group['member'])  # NOT DRY
-            new_group = convert(group, TO_LDAP_MAP)
-            groups_list.append(new_group)
-        except KeyError:
-            logger.error("Problem with Group: " + str(group))
-
-    return groups_list
-
-
-def find_one(name=None, group=None):
-    """ Returns a single group """
-    found_group = None
-    if name is not None:
-        dn = "cn=" + name + "," + BASEDN
-        found_group = manager.find_by_dn(dn)
-
-    if found_group:
-        found_group['member'] = get_names(found_group['member'])  # Repeated in find
-        return convert(found_group, TO_LDAP_MAP)
-
-    if group is not None:
-        fixed_group = convert(group, FROM_LDAP_MAP)
-        found_group = manager.find_one(fixed_group, BASEDN, filter_key="cn")
-
-    if found_group:
-        found_group['member'] = get_names(found_group['member'])  # Repeated in find
-        return convert(found_group, TO_LDAP_MAP)
+    # groups_list = []
+    # for group in groups:
+    #     try:
+    #         group['member'] = get_names(group['member'])  # NOT DRY
+    #         new_group = convert(group, TO_LDAP_MAP)
+    #         groups_list.append(new_group)
+    #     except KeyError:
+    #         logger.error("Problem with Group: " + str(group))
+    return groups
 
 
-def get_names(dn_list):
-    for i, strdn in enumerate(dn_list):
-        dn = ldap.dn.str2dn(strdn)
-        _, name, _ = dn[0][0]
-        dn_list[i] = name
-    return dn_list
-
-
-def delete(name=None, group=None):
-    """ Deletes a group """
-    existing_group = None
-    if name is not None:
-        dn = "cn=" + name + "," + BASEDN
-        existing_group = manager.find_by_dn(dn)
-    elif group is not None:
-        fixed_group = convert(group, FROM_LDAP_MAP)
-        dn = "cn=" + fixed_group['cn'] + "," + BASEDN
-        existing_group = manager.find_one(fixed_group, BASEDN, filter_key="cn")
-
-    if existing_group:
-        manager.delete(dn)
-
-
-def add_member(group_name, member_username):
-    """ should check it the member is the ldap then add them"""
-    group = find_one(group_name)
-    if not group:
-        raise ValueError(str(group_name) + " does not exists")
-    user = users.find_one(member_username)
-    if not user:
-        error_msg = "Trying to add : {0} to {1} but {0} is not in the directory".format(member_username, group_name)
-        logger.error(error_msg)
-        raise ValueError(error_msg)
-
-    if user['username'] not in group['members']:
-        group['members'].append(user['username'])
-        save(group)
-
-
-def remove_member(group_name, member_username):
-    """ should check it the member is the ldap then add them"""
-    group = find_one(group_name)
-    if not group:
-        raise ValueError(str(group_name) + " does not exists")
-
-    if member_username in group['members']:
-        if len(group['members']) == 1:
-            raise ReferenceError("You cannot remove the last member of a group")
-        group['members'].remove(member_username)
-        save(group)
-        logger.info("Removed {0} from {1} group".format(member_username, group_name))
-    else:
-        logger.debug("{0} not found in {1} group".format(member_username, group_name))
+# def find_one(name=None, group=None):
+#     """ Returns a single group """
+#     found_group = None
+#     if name is not None:
+#         dn = "cn=" + name + "," + BASEDN
+#         found_group = manager.find_by_dn(dn)
+#
+#     if found_group:
+#         found_group['member'] = get_names(found_group['member'])  # Repeated in find
+#         return convert(found_group, TO_LDAP_MAP)
+#
+#     if group is not None:
+#         fixed_group = convert(group, FROM_LDAP_MAP)
+#         found_group = manager.find_one(fixed_group, BASEDN, filter_key="cn")
+#
+#     if found_group:
+#         found_group['member'] = get_names(found_group['member'])  # Repeated in find
+#         return convert(found_group, TO_LDAP_MAP)
+#
+#
+# def get_names(dn_list):
+#     for i, strdn in enumerate(dn_list):
+#         dn = ldap.dn.str2dn(strdn)
+#         _, name, _ = dn[0][0]
+#         dn_list[i] = name
+#     return dn_list
+#
+#
+# def delete(name=None, group=None):
+#     """ Deletes a group """
+#     existing_group = None
+#     if name is not None:
+#         dn = "cn=" + name + "," + BASEDN
+#         existing_group = manager.find_by_dn(dn)
+#     elif group is not None:
+#         fixed_group = convert(group, FROM_LDAP_MAP)
+#         dn = "cn=" + fixed_group['cn'] + "," + BASEDN
+#         existing_group = manager.find_one(fixed_group, BASEDN, filter_key="cn")
+#
+#     if existing_group:
+#         manager.delete(dn)
+#
+#
+# def add_member(group_name, member_username):
+#     """ should check it the member is the ldap then add them"""
+#     group = find_one(group_name)
+#     if not group:
+#         raise ValueError(str(group_name) + " does not exists")
+#     user = users.find_one(member_username)
+#     if not user:
+#         error_msg = "Trying to add : {0} to {1} but {0} is not in the directory".format(member_username, group_name)
+#         logger.error(error_msg)
+#         raise ValueError(error_msg)
+#
+#     if user['username'] not in group['members']:
+#         group['members'].append(user['username'])
+#         save(group)
+#
+#
+# def remove_member(group_name, member_username):
+#     """ should check it the member is the ldap then add them"""
+#     group = find_one(group_name)
+#     if not group:
+#         raise ValueError(str(group_name) + " does not exists")
+#
+#     if member_username in group['members']:
+#         if len(group['members']) == 1:
+#             raise ReferenceError("You cannot remove the last member of a group")
+#         group['members'].remove(member_username)
+#         save(group)
+#         logger.info("Removed {0} from {1} group".format(member_username, group_name))
+#     else:
+#         logger.debug("{0} not found in {1} group".format(member_username, group_name))
 
 
 def convert(group, TO_LDAP_MAP):
@@ -151,27 +150,27 @@ def convert(group, TO_LDAP_MAP):
         return group
 
 
-def next_gid_number(yos):
-    """returns the gid number based on the year of study given.
-    The following code applies:
-
-    yos - gid number - name
-     1  - 1000       - first year
-     2  - 2000       - second year
-     3  - 3000       - third year
-     4  - 4000       - fourth year
-        - M Eng      - 4500        - 4599        4500
-     5  - 5000       - postgrad
-     6  - 6000
-        - lecturers  - 6000        - 6099        6000
-        - admin      - 6100        - 6199        6100
-        - technical  - 6200        - 6299        6200
-        - postgrads  - 5000        - 5999        5000
-     7  - 7000       - machine
-
-    """
-    if yos not in range(1, 8):
-        logger.error("Tried to add out of range uid/yos")
-        raise ValueError("The Year of Study: " + str(yos) + " is out of range")
-
-    return yos*1000
+# def next_gid_number(yos):
+#     """returns the gid number based on the year of study given.
+#     The following code applies:
+#
+#     yos - gid number - name
+#      1  - 1000       - first year
+#      2  - 2000       - second year
+#      3  - 3000       - third year
+#      4  - 4000       - fourth year
+#         - M Eng      - 4500        - 4599        4500
+#      5  - 5000       - postgrad
+#      6  - 6000
+#         - lecturers  - 6000        - 6099        6000
+#         - admin      - 6100        - 6199        6100
+#         - technical  - 6200        - 6299        6200
+#         - postgrads  - 5000        - 5999        5000
+#      7  - 7000       - machine
+#
+#     """
+#     if yos not in range(1, 8):
+#         logger.error("Tried to add out of range uid/yos")
+#         raise ValueError("The Year of Study: " + str(yos) + " is out of range")
+#
+#     return yos*1000
