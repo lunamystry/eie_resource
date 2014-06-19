7ad7bdeb0c316909e23508f137534d472abe1a6d
Mandla Mbuli <lm.mbuli@gmail.com>
2014-06-19 18:29:42 +0000

    if a session does not exist, then the person is not authorised

--- backend/admin/rest/utils.py
+++ backend/admin/rest/utils.py
@@ -14,8 +14,11 @@ def admin_only(f):
         # Check if the authentication header is set
         if "x-auth-key" not in request.headers:
             abort(401)
+        logger.info("get session: "+str(request.headers['x-auth-key']))
         session = client.resource.sessions.find_one({
-            'key': request.headers['x-auth-key']})
+            '_id': request.headers['x-auth-key']})
+        if not session:
+            abort(401)
         # Check if the person who the key belongs to is admin
         logger.info(session)
         return f(*args, **kwargs)
