ef81d4f27dad50fad1a16c21dc2653083ceefd97
Mandla Mbuli <lm.mbuli@gmail.com>
2014-06-19 08:43:30 +0000

    I still don't know how to test the rest api but don't print, log

--- backend/tests/admin/groups_rest_tests.py
+++ backend/tests/admin/groups_rest_tests.py
@@ -4,6 +4,9 @@ You much know what is in the database
 """
 import unittest
 import backend
+import logging
+
+logger = logging.getLogger('backend.tests.group_rest_tests')
 
 
 class GroupsRestTestCase(unittest.TestCase):
@@ -15,29 +18,29 @@ class GroupsRestTestCase(unittest.TestCase):
     def test_get_groups(self):
         """ I want to be able to get all groups, assuming database works """
         rv = self.app.get("/groups")
-        print rv.data
+        logger.info(rv.data)
 
     def test_get_group_members(self):
         """ I should be able to get group members """
         rv = self.app.get("/groups/natsuki")
-        print rv.data
+        logger.info(rv.data)
 
     def test_create_group_members(self):
         """ I should be able to add a member to a group """
         rv = self.app.post("/groups/natsuki",
                            content_type="application/json",
                            data='{"username":"guneap"}')
-        print rv.data
+        logger.info(rv.data)
 
     def test_get_group_member(self):
         """ I should be able to add a member to a group """
         rv = self.app.get("/groups/natsuki/guneap")
-        print rv.data
+        logger.info(rv.data)
 
     def test_delete_group_member(self):
         """ I should be able to add a member to a group """
         rv = self.app.delete("/groups/natsuki/guneap")
-        print rv.data
+        logger.info(rv.data)
 
 
 if __name__ == "__main__":
