682c430ee68909d9cb53c049d6531f7d3b0baa7e
Mandla Mbuli <lm.mbuli@gmail.com>
2014-06-21 16:44:23 +0000

    using the alert service on login

--- frontend/js/controllers/home.js
+++ frontend/js/controllers/home.js
@@ -7,19 +7,25 @@ controller.controller('homeCtrl', [
         '$log',
         '$location',
         'SessionUser',
-        function($scope, $log, $location, SessionUser) {
+        'Alerts',
+        function($scope, $log, $location, SessionUser, Alerts) {
+            $scope.alerts = Alerts.alert_list;
+
             $scope.sign_in = function() {
                 if ($scope.login_form.$valid) {
                     SessionUser.sign_in($scope.username, $scope.password,
                         function(value, headers) {
                             $scope.has_error = false;
+                            Alerts.add("info", "You have been logged in");
                             $location.path(SessionUser.homePage);
                         },
                         function(response) {
                             $scope.password = "";
                             if (response.status == 500) {
+                                Alerts.add("danger", "So the server is acting up, tell the webmaster please :-)");
                                 $scope.login_form.error_message = "So the server is acting up, tell the webmaster please :-)";
                             } else {
+                                Alerts.add("danger", "username of password error");
                                 $scope.login_form.error_message = "username of password error";
                             }
                             $scope.has_error = true;
--- frontend/js/services/alerts.js
+++ frontend/js/services/alerts.js
@@ -5,9 +5,9 @@ var service = angular.module('service.alerts', []);
 service.factory('Alerts', [
         '$log', 
         function($log) {
-            alerts = {
-                add: function() {
-
+            var alerts = {
+                add: function(type, message, timeout) {
+                    alerts.alert_list.push({type: type, message: message, timeout:timeout, timedOut: false});
                 },
                 cleanup: function() {
 
