5467c8a66ea6c5c691147ae35984c330736edfb7
Mandla Mbuli <lm.mbuli@gmail.com>
2014-06-21 14:34:58 +0000

    added timeout and close button to the alert

--- frontend/js/directives/alert.js
+++ frontend/js/directives/alert.js
@@ -7,19 +7,26 @@ dir.directive('eieAlert', [
             return {
                 restrict: 'A',
                 replace: true,
-                template: "<div class='alert alert-{{type}}'>"+
+                template: "<div class='alert alert-{{type}}' ng-show='shown()'>"+
                   "{{message}}" +
+                  "<span class='glyphicon glyphicon-remove pull-right' ng-click='close()'></span>"+
+                  "<div class='clearfix'></div>"+
                   "</div>",
                 scope: {
                     message: "@",
                     type: "@",
                     timeout: "@"
                 },
-                controller: ['$scope', '$http', function($scope, $http) {
-                  $scope.timeout = null;
+                controller: ['$scope', '$timeout', function($scope, $timeout) {
+                  var show = true;
                   $scope.type = "info";
                   $scope.message = "";
                   $scope.close = function() {
+                      show = false;
+                  }
+                  $timeout($scope.close, parseInt($scope.timeout, 10));
+                  $scope.shown = function() {
+                        return show;
                   }
                 }],
             }
