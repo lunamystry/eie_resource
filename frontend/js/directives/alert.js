be676f904e62c9118a1d08ac2d067430e6346281
Mandla Mbuli <lm.mbuli@gmail.com>
2014-06-21 16:07:09 +0000

    using a model instead of directly using the attributes

--- frontend/js/directives/alert.js
+++ frontend/js/directives/alert.js
@@ -7,22 +7,31 @@ dir.directive('eieAlert', [
             return {
                 restrict: 'A',
                 replace: true,
-                template: "<div class='alert alert-{{type}}' ng-show='shown()'>"+
-                  "{{message}}" +
+                transclude: true,
+                require: 'ngModel',
+                template: "<div class='alert alert-{{ngModel.type}}' ng-show='shown()'>"+
                   "<span class='glyphicon glyphicon-remove pull-right' ng-click='close()'></span>"+
+                  "<div ng-transclude></div>" +
                   "<div class='clearfix'></div>"+
                   "</div>",
                 scope: {
-                    message: "@",
-                    type: "@",
-                    timeout: "@"
+                    ngModel: '=',
                 },
                 controller: ['$scope', '$timeout', function($scope, $timeout) {
                   var show = true;
+                  if ($scope.ngModel.type == '' || typeof($scope.ngModel.type) == 'undefined') {
+                    $scope.ngModel.type = 'info';
+                  }
+                  if ($scope.ngModel.timeout == '' || typeof($scope.ngModel.timeout) == 'undefined') {
+                    $scope.ngModel.timeout = 5000;
+                  }
                   $scope.close = function() {
                       show = false;
                   }
-                  $timeout($scope.close, parseInt($scope.timeout, 10));
+                  $timeout(function() {
+                      $scope.close();
+                      $scope.ngModel.timedOut = true;
+                  }, parseInt($scope.ngModel.timeout, 10));
                   $scope.shown = function() {
                         return show;
                   }
