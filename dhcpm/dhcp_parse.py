0097e14f144a59e5acd43a6288d299698c67c4da
Mandla Mbuli <lm.mbuli@gmail.com>
2014-07-02 23:02:51 +0000

    added possible parameters as a comment

--- dhcpm/dhcp_parse.py
+++ dhcpm/dhcp_parse.py
@@ -3,6 +3,87 @@
 """
 __version__ = '0.1'
 
+# failover statements:
+#   [ primary | secondary ];
+#   address [ address ];
+#   peer address [ address ];
+#   port [ port-number ];
+#   peer port [ port-number ];
+#   max-response-delay [ seconds ];
+#   max-unacked-updates [ count ];
+#   mclt [ seconds ];
+#   split [ index ];
+#   hba [ colon-separated-hex-list ];
+#   load balance max seconds [ seconds ];
+#   max-lease-misbalance [ percentage ];
+#   max-lease-ownership [ percentage ];
+#   min-balance [ seconds ];
+#   max-balance [ seconds ];
+#
+# range6
+#   range [ dynamic-bootp ] low-address [ high-address]
+#
+# prefix6
+#   prefix6 low-address high-address / bits;
+#
+# parameters
+#   adaptive-lease-time-threshold [ percentage ];
+#   always-broadcast [ flag ];
+#   always-reply-rfc1048 [ flag ];
+#   [authoritative | not authoritative];
+#   boot-unknown-clients [ flag ];
+#   db-time-format [ default | local ];
+#   ddns-hostname [ name ];
+#   ddns-domainname [ name ];
+#   ddns-rev-domainname [ name ];
+#   ddns-update-style [ style ];
+#   ddns-updates [ flag ];
+#   default-lease-time [ time ];
+#   delayed-ack [count];
+#   max-ack-delay [ microseconds ];
+#   do-forward-updates [ flag ];
+#   dynamic-bootp-lease-cutoff [ date ];
+#   dynamic-bootp-lease-length [ length ];
+#   filename [ "filename" ];
+#   fixed-address address [, address ... ];
+#   [ fixed-address6 ip6-address ];
+#   get-lease-hostnames [ flag ];
+#   hardware [ hardware-type ] [ hardware-address ];
+#   host-identifier option [ option-name ] [ option-data ];
+#   infinite-is-reserved [ flag ];
+#   lease-file-name [ name ];
+#   limit-addrs-per-ia [ number ];
+#   dhcpv6-lease-file-name [ name ];
+#   local-port [ port ];
+#   local-address [ address ];
+#   log-facility [ facility ];
+#   max-lease-time [ time ];
+#   min-lease-time [ time ];
+#   min-secs [ seconds ];
+#   next-server [ server-name ];
+#   omapi-port [ port ];
+#   one-lease-per-client [ flag ];
+#   pid-file-name [ name ];
+# 
+#   dhcpv6-pid-file-name [ name ];
+#   ping-check [ flag ];
+#   ping-timeout [ seconds ];
+#   preferred-lifetime [ seconds ];
+#   remote-port [ port ];
+#   server-identifier [ hostname ];
+#   server-duid LLT [ hardware-type timestamp hardware-address ];
+#   server-duid EN enterprise-number enterprise-identifier;
+#   server-duid LL [ hardware-type hardware-address ]; 
+#   server-name [ name ]; 
+#   site-option-space [ name ];
+#   stash-agent-options [ flag ];
+#   update-conflict-detection [ flag ];
+#   update-optimization [ flag ];
+#   update-static-leases [ flag ];
+#   use-host-decl-names [ flag ];
+#   use-lease-addr-for-default-route [ flag ];
+#   vendor-option-space [ string ];
+
 
 def extract_parameters(config_lines):
     """
@@ -91,13 +172,13 @@ def extract_options(config_lines):
             options.append({"name": name, "values": values})
     return options
 
-
-def subnets(config_lines):
-    """
-        assume:
-            * subnets take up multiple lines and are not closed when are opened
-
-        input: a list of lines
-        return: a list of subnets with its options and groups
-    """
-    pass
+def extract_allow_deny_ignore(config_lines):
+    params = []
+    for line in config_lines:
+        line = line.lstrip()
+        if line.startswith('allow') or
+            line.startswith('deny') or
+            line.startswith('ignore'):
+                words = line.split(" ")
+                params.append({words[0]: words[1]})
+    return params
