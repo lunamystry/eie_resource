c4c9d0f42a8e9249f2baaccf299b641957aa7048
Mandla Mbuli <lm.mbuli@gmail.com>
2014-07-02 20:04:49 +0000

    starting to write a parser for the dhcp.conf file which will work the way I want

---
+++ dhcpm/dhcp_parse.py
@@ -0,0 +1,64 @@
+"""
+    a dhcp parser cause I can't find one that I like or works the way I want.
+"""
+__version__ = '0.1'
+
+import re
+
+
+def extract_parameters(config_lines):
+    """
+        assume:
+            * one parameter per line
+            * comments are not important so just ignore
+            * parameters are only at the top, so as soon as a line containing
+              '{' is encountered or the end of the string is reached, then the
+              extraction stops
+
+        input: config string
+        return: a list of parameters, Each parameter is a tuple with name and
+                value.
+    """
+    pass
+
+
+def extract_groups(config_lines):
+    """
+        Search for all lines that have '{' and return:
+            {'parameters': [('name1', 'value1'), ('name2', 'value2')],
+             'declarations': [declaration1, declaration2]}
+
+        assume:
+            * parameters always come first
+            * declarations are like groups (name, values)
+
+        input: config string
+        return: a list of groups.
+    """
+    pass
+
+
+def strip_comments(config_lines):
+    """
+
+    """
+    comments = []
+    for line in config_lines:
+        if re.search(r'#'):
+            pass
+
+
+def options(config_lines):
+    """
+        input: a list of lines from the config file
+        return: a list of options from those lines
+    """
+    options = []
+    for line in config_lines:
+        if re.search(r'option', line):
+            words = line.split(" ")
+            name = words[1]
+            values = "".join(words[2:]).split(',')
+            options.append({"name": name, "values": values})
+    return options
+
