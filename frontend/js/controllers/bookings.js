2dbe1271b6a27cb44b0583e4e5a2109c5f34f40f
Mandla Mbuli <lm.mbuli@gmail.com>
2014-07-04 21:26:28 +0000

    dummy computers data

--- frontend/js/controllers/bookings.js
+++ frontend/js/controllers/bookings.js
@@ -6,5 +6,32 @@ controller.controller('bookingsCtrl', [
         "$scope",
         "$log",
         function ($scope, $log) {
+            $scope.computers = [{"name": "dlab1",
+                                 "number": 1,
+                                 "eietag": "0000",
+                                 "ipv4": "0.0.0.0",
+                                 "mac": "0:0:0:0:0",
+                                 "booked_date": null,
+                                 "status": "absent",  // faulty | available | unknown
+                                 "software": [],
+                                 "comment": "not in the lab"},
+                                {"name": "dlab19",
+                                "number": 19,
+                                "eietag": "0000",
+                                "ipv4": "0.0.0.0",
+                                "mac": "0:0:0:0:0",
+                                "booked_date": null,
+                                "status": "available",
+                                "software": [],
+                                "comment": "not in the lab"},
+                                {"name": "dlab1",
+                                "number": 43,
+                                "eietag": "0000",
+                                "ipv4": "0.0.0.0",
+                                "mac": "0:0:0:0:0",
+                                "booked_date": null,
+                                "status": "faulty",  // faulty | available | unknown
+                                "software": [],
+                                "comment": "not in the lab"}]
         }
 ]);
