012ccdc2843f296543184d64c6e248da7229cec1
Mandla Mbuli <lm.mbuli@gmail.com>
2014-07-04 22:27:29 +0000

    adding functions to return list of computers for the different sections

--- frontend/js/services/computers.js
+++ frontend/js/services/computers.js
@@ -2,10 +2,10 @@
 
 var service = angular.module('service.computers', []);
 
-service.factory('Computers', [
-        '$log', 
-        '$cookieStore', 
-        '$resource', 
+service.factory('Computer', [
+        '$log',
+        '$cookieStore',
+        '$resource',
         function($log, $cookieStore, $resource) {
             var key = $cookieStore.get('session_id');
             return $resource('/computers/:_id',
@@ -20,3 +20,31 @@ service.factory('Computers', [
                 )
         }
 ]);
+service.factory('Computers', [
+        '$log',
+        'Computer',
+        function($log, Computer) {
+            var computers = {
+                all_computers: null,
+                all: function(successFn, errorFn) {
+                    if (computers.all_computers == null) {
+                        Computer.query(function(value, header){
+                        }, function(response) {
+
+                        });
+                    } else {
+                        return computers.all_computers;
+                    }
+                },
+                first_col: function(successFn, errorFn) {
+
+                },
+                second_col: function(successFn, errorFn) {
+
+                },
+                third_col: function(successFn, errorFn) {
+
+                }
+            };
+            return computers;
+}]);
