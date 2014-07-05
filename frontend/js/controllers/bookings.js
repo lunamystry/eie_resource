814beb3f936f45b9ca1e279fc068c7454a0eabec
Mandla Mbuli <lm.mbuli@gmail.com>
2014-07-05 16:22:39 +0000

    using computer service and have the number of computer correct in each layout

--- frontend/js/controllers/bookings.js
+++ frontend/js/controllers/bookings.js
@@ -5,22 +5,10 @@ var controller = angular.module('controller.bookings', []);
 controller.controller('bookingsCtrl', [
         "$scope",
         "$log",
-        function ($scope, $log) {
-            $scope.computers = [{"name": "dlab1",
-                                 "comment": "not in the lab"},
-                                {"name": "dlab2",
-                                "comment": "not in the lab"},
-                                {"name": "dlab44",
-                                "comment": "not in the lab"},
-                                {"name": "dlab45",
-                                "comment": "not in the lab"},
-                                {"name": "dlab28",
-                                "comment": "not in the lab"},
-                                {"name": "dlab19",
-                                "comment": "not in the lab"},
-                                {"name": "dlab20",
-                                "comment": "not in the lab"},
-                                {"name": "dlab43",
-                                "comment": "not in the lab"}]
+        "Computers",
+        function ($scope, $log, Computers) {
+            $scope.section1 = Computers.sections()[0];
+            $scope.section2 = Computers.sections()[1];
+            $scope.section3 = Computers.sections()[2];
         }
 ]);
--- frontend/js/services/computers.js
+++ frontend/js/services/computers.js
@@ -25,25 +25,72 @@ service.factory('Computers', [
         'Computer',
         function($log, Computer) {
             var computers = {
-                all_computers: null,
-                all: function(successFn, errorFn) {
+                all_computers: [],
+                sections: function(successFn, errorFn) {
                     if (computers.all_computers == null) {
                         Computer.query(function(value, header){
                         }, function(response) {
 
                         });
                     } else {
-                        return computers.all_computers;
+                        var section1 = [{"name": "dlab1",
+                                         "comment": "not in the lab"},
+                                        {"name": "dlab2",
+                                        "comment": "not in the lab"},
+                                        {"name": "dlab3",
+                                        "comment": "not in the lab"},
+                                        {"name": "dlab4",
+                                        "comment": "not in the lab"},
+                                        {"name": "dlab5",
+                                        "comment": "not in the lab"},
+                                        {"name": "dlab6",
+                                        "comment": "not in the lab"},
+                                        {"name": "dlab7",
+                                        "comment": "not in the lab"},
+                                        {"name": "dlab8",
+                                        "comment": "not in the lab"}];
+                        var section2 = [{"name": "dlab19",
+                                         "comment": "not in the lab"},
+                                        {"name": "dlab20",
+                                        "comment": "not in the lab"},
+                                        {"name": "dlab21",
+                                        "comment": "not in the lab"},
+                                        {"name": "dlab22",
+                                        "comment": "not in the lab"},
+                                        {"name": "dlab23",
+                                        "comment": "not in the lab"},
+                                        {"name": "dlab24",
+                                        "comment": "not in the lab"},
+                                        {"name": "dlab25",
+                                        "comment": "not in the lab"},
+                                        {"name": "dlab26",
+                                        "comment": "not in the lab"}];
+                        var section3 = [{"name": "dlab43",
+                                         "comment": "not in the lab"},
+                                        {"name": "dlab44",
+                                        "comment": "not in the lab"},
+                                        {"name": "dlab45",
+                                        "comment": "not in the lab"},
+                                        {"name": "dlab46",
+                                        "comment": "not in the lab"},
+                                        {"name": "dlab47",
+                                        "comment": "not in the lab"},
+                                        {"name": "dlab48",
+                                        "comment": "not in the lab"},
+                                        {"name": "dlab49",
+                                        "comment": "not in the lab"},
+                                        {"name": "dlab50",
+                                        "comment": "not in the lab"}];
+                        var sections = [section1, section2, section3]
+                        return sections;
                     }
                 },
                 first_col: function(successFn, errorFn) {
-
                 },
                 second_col: function(successFn, errorFn) {
-
                 },
                 third_col: function(successFn, errorFn) {
-
+                    return section3;
                 }
             };
             return computers;
