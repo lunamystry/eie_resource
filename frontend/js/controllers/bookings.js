c46953929cb9e343d5daadac9208999a672b1a08
Mandla Mbuli <lm.mbuli@gmail.com>
2014-07-12 21:37:06 +0000

    got the color to change by status

--- frontend/js/controllers/bookings.js
+++ frontend/js/controllers/bookings.js
@@ -14,26 +14,32 @@ controller.controller('bookingsCtrl', [
             Computer.query(null, function(response, headers) {
                 var section1 = [];
                 for (var i = 1; i < 19; i++) {
-                    if (typeof(response[i - 1]) !== 'undefined' && response[i - 1].number == i) {
-                        $scope.section1.push(response[i - 1]);
-                    } else {
-                        $scope.section1.push({"name": "dlab"+i});
+                    $scope.section1.push({"name": "dlab"+i});
+                }
+                for (var i = 0; i < response.length; i++) {
+                    var n = response[i].number;
+                    if (n > 0 && n < 19) {
+                        $scope.section1[n - 1] = response[i];
                     }
                 }
                 var section2 = [];
                 for (var i = 19; i < 43; i++) {
-                    if (typeof(response[i - 1]) !== 'undefined' && response[i - 1].number == i) {
-                        $scope.section2.push(response[i - 1]);
-                    } else {
-                        $scope.section2.push({"name": "dlab"+i});
+                    $scope.section2.push({"name": "dlab"+i});
+                }
+                for (var i = 0; i < response.length; i++) {
+                    var n = response[i].number;
+                    if (n > 18 && n < 43) {
+                        $scope.section2[n - 19] = response[i];
                     }
                 }
                 var section3 = [];
                 for (var i = 43; i < 78; i++) {
-                    if (typeof(response[i - 1]) !== 'undefined' && response[i - 1].number == i) {
-                        $scope.section3.push(response[i - 1]);
-                    } else {
-                        $scope.section3.push({"name": "dlab"+i});
+                    $scope.section3.push({"name": "dlab"+i});
+                }
+                for (var i = 0; i < response.length; i++) {
+                    var n = response[i].number;
+                    if (n > 42 && n < 78) {
+                        $scope.section3[n - 43] = response[i];
                     }
                 }
             }, function (response) {
