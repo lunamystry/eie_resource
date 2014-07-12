4684b565c5274dcb834d9181f14b4fff8e627810
Mandla Mbuli <lm.mbuli@gmail.com>
2014-07-12 20:58:17 +0000

    moved the section division to the bookings controller

--- frontend/js/controllers/bookings.js
+++ frontend/js/controllers/bookings.js
@@ -5,10 +5,39 @@ var controller = angular.module('controller.bookings', []);
 controller.controller('bookingsCtrl', [
         "$scope",
         "$log",
-        "Computers",
-        function ($scope, $log, Computers) {
-            $scope.section1 = Computers.sections()[0];
-            $scope.section2 = Computers.sections()[1];
-            $scope.section3 = Computers.sections()[2];
+        "Computer",
+        function ($scope, $log, Computer) {
+            $scope.section1 = [];
+            $scope.section2 = [];
+            $scope.section3 = [];
+
+            Computer.query(null, function(response, headers) {
+                var section1 = [];
+                for (var i = 1; i < 19; i++) {
+                    if (typeof(response[i - 1]) !== 'undefined' && response[i - 1].number == i) {
+                        $scope.section1.push(response[i - 1]);
+                    } else {
+                        $scope.section1.push({"name": "dlab"+i});
+                    }
+                }
+                var section2 = [];
+                for (var i = 19; i < 43; i++) {
+                    if (typeof(response[i - 1]) !== 'undefined' && response[i - 1].number == i) {
+                        $scope.section2.push(response[i - 1]);
+                    } else {
+                        $scope.section2.push({"name": "dlab"+i});
+                    }
+                }
+                var section3 = [];
+                for (var i = 43; i < 78; i++) {
+                    if (typeof(response[i - 1]) !== 'undefined' && response[i - 1].number == i) {
+                        $scope.section3.push(response[i - 1]);
+                    } else {
+                        $scope.section3.push({"name": "dlab"+i});
+                    }
+                }
+            }, function (response) {
+                console.log("failure");
+            })
         }
 ]);
--- frontend/js/services/computers.js
+++ frontend/js/services/computers.js
@@ -40,35 +40,3 @@ service.factory('Computer', [
                 )
         }
 ]);
-service.factory('Computers', [
-        '$log',
-        'Computer',
-        function($log, Computer) {
-            var computers = {
-                all_computers: [],
-                sections: function(successFn, errorFn) {
-                    if (computers.all_computers == null) {
-                        Computer.query(function(value, header){
-                        }, function(response) {
-
-                        });
-                    } else {
-                        var section1 = [];
-                        for (var i = 1; i < 19; i++) {
-                            section1.push({"name": "dlab"+i});
-                        }
-                        var section2 = [];
-                        for (var i = 19; i < 43; i++) {
-                            section2.push({"name": "dlab"+i});
-                        }
-                        var section3 = [];
-                        for (var i = 43; i < 78; i++) {
-                            section3.push({"name": "dlab"+i});
-                        }
-                        var sections = [section1, section2, section3]
-                        return sections;
-                    }
-                },
-            };
-            return computers;
-}]);
