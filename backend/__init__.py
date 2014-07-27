d28c17c49730f814c0550ebb9ab57df845d6b84e
Mandla Mbuli <lm.mbuli@gmail.com>
2014-07-27 10:38:12 +0000

    Using a function to setup the application and loggging

--- backend/__init__.py
+++ backend/__init__.py
@@ -2,25 +2,57 @@ from flask import Flask
 from werkzeug.contrib.fixers import ProxyFix
 from flask.ext.restful import Api
 import os
+import json
+import logging
+import logging.config
 
 
-app = Flask(__name__,
-            template_folder='../frontend/views',
-            static_url_path='',
-            static_folder='../frontend')
+def _setup_logging(
+        default_path='/etc/eie_config/logging_config.json',
+        default_level=logging.DEBUG,
+        env_key='RESOURCE_LOG_CFG'
+        ):
+    """Setup logging configuration
+
+    """
+    path = default_path
+    value = os.getenv(env_key, None)
+    if value:
+        path = value
+    if os.path.exists(path):
+        with open(path, 'rt') as f:
+            config = json.loads(f.read())
+        logging.config.dictConfig(config)
+    else:
+        error_msg = "{} does not exist".format(path)
+        logging.warn(error_msg)
+        logging.basicConfig(level=default_level)
 
 
-config_dir = "/etc/eie_config"
-try:
-    filename = os.path.join(config_dir, "resource.cfg")
-    with open(filename) as source:
-        app.config.from_pyfile(filename)
-except IOError:
-    app.logger.error("No application configuration in: " + config_dir)
+def _setup_app(
+        default_path='/etc/eie_config/resource.cfg',
+        env_key='RESOURCE_LOG_CFG'
+        ):
+    """Setup application configuration
+
+    """
+    app = Flask(__name__,
+                template_folder='../frontend/views',
+                static_url_path='',
+                static_folder='../frontend')
+    path = default_path
+    if os.path.exists(path):
+        app.config.from_pyfile(path)
+    else:
+        error_msg = "{} does not exist".format(path)
+        logging.error(error_msg)
+        raise IOError(error_msg)
+    return app
 
 
+_setup_logging()
+app = _setup_app()
 app.wsgi_app = ProxyFix(app.wsgi_app)
 api = Api(app)
-config = app.config
 
 from backend import routes
