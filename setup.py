3aa56b48c20298d6c76dfd408f3e59f326654f02
Mandla Mbuli <lm.mbuli@gmail.com>
2014-07-29 11:18:14 +0000

    version 0.15

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='Resource',
-    version='v0.14.2',
+    version='v0.15',
     long_description=__doc__,
     packages=['backend', 'eieldap'],
     include_package_data=True,
