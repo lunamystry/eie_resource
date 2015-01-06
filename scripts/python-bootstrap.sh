22ed7e6faa89c19693103a11a8b701aa5c3c5fb0
Mandla Mbuli <lm.mbuli@gmail.com>
2015-01-06 11:05:24 +0200

    put everything inside a virtualenv directory and activate the initial env

--- scripts/python-bootstrap.sh
+++ scripts/python-bootstrap.sh
@@ -9,13 +9,28 @@ ENV_OPTS='--no-site-packages --distribute'
 # Set to whatever python interpreter you want for your first environment:
 PYTHON=$(which python)
 URL_BASE=http://pypi.python.org/packages/source/v/virtualenv
+VIRTUALENV_DIR=$HOME/.bin/virtualenvironments
 
 # --- Real work starts here ---
+
+cur_dir=pwd
+
+if [ ! -d $VIRTUALENV_DIR ]; then
+    mkdir -p $VIRTUALENV_DIR
+fi
+cd $VIRTUALENV_DIR
+
 wget $URL_BASE/virtualenv-$VERSION.tar.gz
 tar xzf virtualenv-$VERSION.tar.gz
 # Create the first "bootstrap" environment.
 $PYTHON virtualenv-$VERSION/virtualenv.py $ENV_OPTS $INITIAL_ENV
-# Don't need this anymore.
-rm -rf virtualenv-$VERSION
 # Install virtualenv into the environment.
 $INITIAL_ENV/bin/pip install virtualenv-$VERSION.tar.gz
+# Don't need this anymore.
+rm -rf virtualenv-$VERSION
+rm -rf virtualenv-$VERSION.tar.gz
+
+#activate the initial env
+sh $VIRTUALENV_DIR/$INITIAL_ENV/bin/activate
+
+cd $curr_dir
