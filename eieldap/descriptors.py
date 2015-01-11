1e810a2285b79ae19f7d2c569e15c888b07846ad
Mandla Mbuli <lm.mbuli@gmail.com>
2015-01-11 14:34:12 +0200

    made descriptors use inheritance for resolution of MRO

--- eieldap/descriptors.py
+++ eieldap/descriptors.py
@@ -27,68 +27,75 @@ class Typed(Descriptor):
         super(Typed, self).__set__(instance, value)
 
 
-class String(Typed):
-    ty = str
-
-    def __init__(self, min=None, max=None):
+class Ranged(Descriptor):
+    def __init__(self, min=None, max=None, **kwargs):
         self.min = min
         self.max = max
-        super(String, self).__init__()
-
-    def __set__(self, instance, value):
-        ''' This will ensure that the min and max values are proper and that the
-        type is a string
-        '''
-        if self.min and len(value) < self.min:
-            raise TypeError('Must be >= %d' % self.min)
-        if self.max and len(value) > self.max:
-            raise TypeError('Must be <= %d' % self.max)
-        super(String, self).__set__(instance, value)
-
-
-class YearOfStudy(Typed):
-    ty = int
-
-    def __init__(self, min, max):
-        self.min = min
-        self.max = max
-        super(YearOfStudy, self).__init__()
+        super(Ranged, self).__init__(**kwargs)
 
     def __set__(self, instance, value):
         if self.min and value < self.min:
             raise TypeError('Must be >= %d' % self.min)
         if self.max and value > self.max:
             raise TypeError('Must be <= %d' % self.max)
-        super(YearOfStudy, self).__set__(instance, value)
+        super(Ranged, self).__set__(instance, value)
 
 
-class IntString(String):
+class Sized(Descriptor):
+    def __init__(self, min=None, max=None, **kwargs):
+        self.min = min
+        self.max = max
+        super(Sized, self).__init__(**kwargs)
+
+    def __set__(self, instance, value):
+        if self.min and len(value) < self.min:
+            raise TypeError('Must be >= %d' % self.min)
+        if self.max and len(value) > self.max:
+            raise TypeError('Must be <= %d' % self.max)
+        super(Sized, self).__set__(instance, value)
+
+
+class String(Typed):
+    ty = str
+
+
+class SizedString(String, Sized):
+    pass
+
+
+class IntSizedString(SizedString):
     '''
     A string that can be converted to an integer
     '''
     def __init__(self, **kwargs):
-        super(IntString, self).__init__(**kwargs)
+        super(IntSizedString, self).__init__(**kwargs)
 
     def __set__(self, instance, value):
         try:
             int(value)
         except ValueError:
             raise ValueError('Must be convertable to integer' % self.max)
-        super(IntString, self).__set__(instance, value)
+        super(IntSizedString, self).__set__(instance, value)
 
 
-class PasswordString(String):
+class Regex(Descriptor):
+    def __init__(self, *args, **kwargs):
+        self.pattern = re.compile(kwargs.pop('pattern'))
+        super(Regex, self).__init__(*args, **kwargs)
+
     def __set__(self, instance, value):
-        ''' Password rules:
-        1. No special characters allowed
-        2. Space not allowed
-        3. Min 6 Max 20
-        4. At least one numeric character
-        5. At least one capital letter
-        6. Only two repetitive characters allowed
-        '''
-        s = '^(?=.*[A-Z])(?=.*\d)(?!.*(.)\1\1)[a-zA-Z0-9@]{6,20}$'
-        print(s)
-        if not re.match(s, value):
-            raise TypeError('Password does not meet minimum requirements')
-        super(PasswordString, self).__set__(instance, value)
+        if not self.pattern.match(value):
+            raise ValueError('Invalid string')
+        super(Regex, self).__set__(instance, value)
+
+
+class YearOfStudy(Typed, Ranged):
+    ty = int
+
+
+class PasswordString(SizedString, Regex):
+    pass
+
+
+class RegexString(String, Regex):
+    pass
