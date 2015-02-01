21a6be55ca84825318cba5751b0de71db595824f
Mandla Mbuli <lm.mbuli@gmail.com>
2015-02-01 10:26:40 +0200

    can list users now

--- scripts/useradd.py
+++ scripts/useradd.py
@@ -6,6 +6,7 @@ sys.path.append('..')
 import argparse
 
 from eieldap import importer
+from eieldap import users
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
@@ -17,9 +18,18 @@ if __name__ == "__main__":
                         help='the path to the xls filename')
     parser.add_argument('-s', '--sync', action='store_true',
                         help='remove users which are not in the xls file')
+    parser.add_argument('-l', '--list', action='store_true',
+                        help='list all the users in the ldap')
 
     args = parser.parse_args()
     if args.xlsfilename:
         importer.add_from_xls(str(args.xlsfilename), args.sync)
+    elif args.list:
+        user_list = users.User.find()
+        for user in user_list:
+            print("{} ({} {}) - {}".format(user.username,
+                                           user.first_name,
+                                           user.last_name,
+                                           user.year_of_study))
     else:
         parser.print_usage()
